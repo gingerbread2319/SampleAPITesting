28e3de91803db650334866530cb35e1ef977e1c9
Leandro Arellano <gingerbread2318@gmail.com>
2025-03-16 16:28:02 +0800

    Remove unused imports in APIUtils.cs and ExtentReport.cs

--- Core/ExtentReport.cs
+++ Core/ExtentReport.cs
@@ -1,11 +1,6 @@
 ﻿using AventStack.ExtentReports.Reporter.Configuration;
 using AventStack.ExtentReports.Reporter;
 using AventStack.ExtentReports;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SampleAPITestProject.Core
 {
